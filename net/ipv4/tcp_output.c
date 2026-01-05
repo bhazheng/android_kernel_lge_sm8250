42f69bbe71a07a0f89660e553c985f4e54406f81
bhazheng <bhazheng@gmail.com>
2026-01-05 19:19:29 +0700

    tcp: Remove redundant tcp_mstamp_refresh function implementation

--- net/ipv4/tcp_output.c
+++ net/ipv4/tcp_output.c
@@ -60,22 +60,6 @@ void tcp_mstamp_refresh(struct tcp_sock *tp)
 		tp->tcp_mstamp = val;
 }
 
-/* Refresh clocks of a TCP socket,
- * ensuring monotically increasing values.
- */
-void tcp_mstamp_refresh(struct tcp_sock *tp)
-{
-	u64 val = tcp_clock_ns();
-
-	if (val > tp->tcp_clock_cache)
-		tp->tcp_clock_cache = val;
-
-	val = div_u64(val, NSEC_PER_USEC);
-	if (val > tp->tcp_mstamp)
-		tp->tcp_mstamp = val;
-}
-
-
 static bool tcp_write_xmit(struct sock *sk, unsigned int mss_now, int nonagle,
 			   int push_one, gfp_t gfp);
 
