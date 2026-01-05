6ea5abab2908b0844a8ccd4c15888e99fffddd29
bhazheng <bhazheng@gmail.com>
2026-01-05 20:39:05 +0700

    bpf: Fix incorrect variable name in memory management functions

--- kernel/bpf/core.c
+++ kernel/bpf/core.c
@@ -913,8 +913,8 @@ bpf_jit_binary_alloc(unsigned int proglen, u8 **image_ptr,
 void bpf_jit_binary_free(struct bpf_binary_header *hdr)
 {
 	u32 pages = hdr->pages;
-	set_memory_rw((unsigned long)header, header->pages);
-	set_memory_nx((unsigned long)header, header->pages);
+	set_memory_rw((unsigned long)hdr, hdr->pages);
+	set_memory_nx((unsigned long)hdr, hdr->pages);
 	bpf_jit_free_exec(hdr);
 	bpf_jit_uncharge_modmem(pages);
 }
