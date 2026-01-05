01d13b3777a8cbf12aa0253ca9d5c1ef8323acf6
bhazheng <bhazheng@gmail.com>
2026-01-05 13:52:59 +0700

    bpf: Remove duplicate inclusion of <linux/btf.h> and unused <linux/frame.h>

--- kernel/bpf/core.c
+++ kernel/bpf/core.c
@@ -25,8 +25,6 @@
 #include <linux/moduleloader.h>
 #include <linux/bpf.h>
 #include <linux/btf.h>
-#include <linux/frame.h>
-#include <linux/btf.h>
 #include <linux/objtool.h>
 #include <linux/rbtree_latch.h>
 #include <linux/kallsyms.h>
