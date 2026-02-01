0b8e72439dede21387da763ca251ab1cffcc8f6e
bhazheng <bhazheng@gmail.com>
2026-02-01 08:30:10 +0700

    remove is_ksu_transition

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -2212,10 +2212,6 @@ static u32 ptrace_parent_sid(void)
 
 	return sid;
 }
-#ifdef CONFIG_KSU_MANUAL_HOOK
-extern bool is_ksu_transition(const struct task_security_struct *old_tsec, 
-				const struct task_security_struct *new_tsec);
-#endif
 
 static int check_nnp_nosuid(const struct linux_binprm *bprm,
 			    const struct task_security_struct *old_tsec,
@@ -2253,10 +2249,6 @@ static int check_nnp_nosuid(const struct linux_binprm *bprm,
     }
 #endif
 
-#ifdef CONFIG_KSU_MANUAL_HOOK
-	if (is_ksu_transition(old_tsec, new_tsec))
-		return 0;
-#endif
 
 	/*
 	 * If the policy enables the nnp_nosuid_transition policy capability,
