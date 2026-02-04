8b9fa9a533bf2f24a961fd7b9ec6819e1bae351e
bhazheng <bhazheng@gmail.com>
2026-02-04 09:40:28 +0700

    fs: susfs: Replaced susfs_is_boot_completed_triggered with susfs_is_sdcard_android_data_decrypted  Author: sidex15 <24408329+sidex15@users.noreply.github.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1772,7 +1772,7 @@ static int exec_binprm(struct linux_binprm *bprm)
 #ifdef CONFIG_KSU_SUSFS
 extern bool ksu_execveat_hook __read_mostly;
 extern bool ksu_su_compat_enabled __read_mostly;
-extern bool susfs_is_boot_completed_triggered __read_mostly;
+extern bool susfs_is_sdcard_android_data_decrypted __read_mostly;
 extern bool __ksu_is_allow_uid_for_current(uid_t uid);
 extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
 			void *envp, int *flags);
@@ -1797,7 +1797,7 @@ static int __do_execve_file(int fd, struct filename *filename,
 	if (likely(susfs_is_current_proc_umounted()) || !ksu_su_compat_enabled) {
 		goto orig_flow;
 	}
-	if (unlikely(ksu_execveat_hook || !susfs_is_boot_completed_triggered)) {
+	if (unlikely(ksu_execveat_hook || !susfs_is_sdcard_android_data_decrypted)) {
 		ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
 	} else if ((__ksu_is_allow_uid_for_current(current_uid().val))) {
 		ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);
